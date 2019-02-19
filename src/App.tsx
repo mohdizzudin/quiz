2d6779a4e6e5914af6d5295f7032fc0092ce0540
AzrizHaziq <azrizhaziq@gmail.com>
2019-02-19 21:28:03 +0800

    * added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import './App.scss';
-import logo from './logo.svg';
+import './App.scss'
+import logo from './logo.svg'
 import { hot } from 'react-hot-loader'
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default process.env.NODE_ENV === "development" ? hot(module)(App) : App
+export default (process.env.NODE_ENV === 'development' ? hot(module)(App) : App)
