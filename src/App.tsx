5b8502e3bcc5dd3a04fb9b6ade77e35498770e7f
AzrizHaziq <azrizhaziq@gmail.com>
2019-02-19 22:19:21 +0800

    * added answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import { QuestionAnswer } from './components/question-answer/question-answer'
 import { QuestionCard } from './components/question-card/question-card'
 import { useQuestionNumber } from './useHook/question-number.state'
 
@@ -13,9 +14,10 @@ function App() {
         <div className="row flex-grow-1 justify-content-center">
           <div className="col-8">
             <QuestionCard>
-              <h4>{questionNumber}) As-Sajda</h4>
-              <p className='mb-0 d-flex align-self-end'>Meccan Surah</p>
+              <h5>{questionNumber}. As-Sajda</h5>
+              <small className='mb-0 d-flex align-self-end'>Meccan Surah</small>
             </QuestionCard>
+            <QuestionAnswer/>
           </div>
         </div>
       </div>
