a95d574509f010597ef1116a8e0e569a97c36f4c
AzrizHaziq <azrizhaziq@gmail.com>
2019-02-19 20:27:02 +0800

    git init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import { hot } from 'react-hot-loader'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App)
